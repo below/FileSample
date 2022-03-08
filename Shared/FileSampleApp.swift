fab58214cc43eee6818c61d26dbc8b91d012a7cc
Alexander von Below <alex@vonbelow.com>
2022-03-08 22:36:41 +0100

    Initial Commit

---
+++ Shared/FileSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSampleApp.swift
+//  Shared
+//
+//  Created by Alexander von Below on 08.03.22.
+//
+
+import SwiftUI
+
+@main
+struct FileSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
